2fe889cbe89a8d33ff0a453ee263a9b68f4652a3
Emmanuel Okyere <chief@hutspace.net>
2014-08-07 06:31:51 +0000

    Added original datasets and centres.xls to be used to consolidate all data

---
+++ README.md
@@ -0,0 +1,4 @@
+The Electoral Commission of Ghana is undertaking a Registration Excercise.
+
+This is a quick project to normalize data for designated registration centres,
+so we can generate a quick CSV to be used with a GotToVote.cc instance for Ghana.
---
+++ centres.py
@@ -0,0 +1,19 @@
+from xlrd import open_workbook
+
+WB = '/home/eokyere/Desktop/EC/data/centres.xls'
+
+def main():
+    pass
+
+
+def upper_east():
+    """District name, then rows of EAs and Centers
+    Total 353
+    """
+
+
+def wb(fname):
+    pass
+
+def test():
+    pass
