06cce7cdd867c409f8de66e70daa0177706fa87a
Emmanuel Okyere <chief@hutspace.net>
2014-08-08 06:05:22 +0000

    Cleaned up Northern data; minor updates and documentation for parser

--- centres.py
+++ centres.py
@@ -13,20 +13,22 @@ from xlrd import open_workbook
 DISTRICT_PATTERN = r'^\s*DISTRICT\s*:\s*(\w[\w\s\-\.]+)\s*$'
 
 HEADER_PATTERNS = (
-    r'.*ELECTORAL\s+AREA.*',
+    r'.*ELECTORAL.*',
     r'.*DESIGNATE.*',
     r'.*REGISTRATION.*',
     r'.*E\s*/\s*A.*',
     r'\s+CODE[\s/]+'
 )
 
-# WB = '/home/eokyere/Desktop/EC/data/centres.xls'
-
 DISTRICT, DATA, SKIP = range(3)
+
 
 def main(args):
     data = handle(args[1])
     writer = csv.writer(sys.stdout)
+
+    f = lambda a, b: a + b
+    data = sorted(data, key=lambda x: reduce(f, [s.lower() for s in x]))
 
     for line in data:
         writer.writerow([s.encode('utf8') for s in line])
@@ -34,78 +36,96 @@ def main(args):
     sys.exit(0)
 
 
-def handle(src, region=None):
-    """Given a folder, find all xls, xlsx sheets and parse
-    First sheets (0) found
+def handle(src, region=None, depth=0, limit=0):
+    """Given a source, if it is an xls(x) file, parse it.
+    If it is a folder, find all xls(x) files and parse them.
     """
     if not region:
         region = fname(src)
 
+    region = region.title()
+
     if os.path.isfile(src) and is_xls(src):
-        lines = scan(src)
-        return parse(src, lines, region)
-    elif os.path.isdir(src):
+        return parse(scan(src), region, fname(src).title())
+    elif os.path.isdir(src) and depth <= limit:
         xs = []
+        depth += 1
         for f in os.listdir(src):
             path = os.path.abspath(os.path.join(src, f))
-            xs.extend(handle(path, fname(src)))
+            xs.extend(handle(path, fname(src), depth, limit))
         return xs
+    return []
 
 def scan(src):
+    """Analyze all rows of the workbook to determine which rows to extract 
+    data from.
+
+    :param: src - the filename fo the Excel sheet to scan
+    :return: Returns a generator of (kind, value) tuples
+    """
     wb = open_wb(src)
     sheet = wb.sheets()[0]
     return (scan_row(sheet.row_values(i)) for i in range(sheet.nrows))
 
 def scan_row(row):
-    # try:
-    if len(row) >= 3 and row[1] and row[2]:
-        for p in HEADER_PATTERNS:
-            if re.match(p, row[1], flags=re.I) or re.match(p, row[2], flags=re.I):
-                break
-        else:
-            return (DATA, [' '.join(x.strip().split()) for x in row[1:3]])
-    elif len(row) >= 1 and row[0]:
-        m = re.match(DISTRICT_PATTERN, unicode(row[0]), flags=re.I|re.UNICODE)
-        if m:
-            return (DISTRICT, m.group(1).strip())
-    # except Exception, e:
-        # print '>>>>', e
-        # raise e
+    """Analyze a row to determine whether we should parse it, and what kind of row it is.
+
+    :param: row - Row values from Excel sheet
+    :return: Returns a tuple of (kind, value) -- kinds: DISTRICT, DATA, SKIP
+    """
+    try:
+        if len(row) >= 3 and row[1] and row[2]:
+            for p in HEADER_PATTERNS:
+                if re.match(p, row[1], flags=re.I) or re.match(p, row[2], flags=re.I):
+                    break
+            else:
+                return (DATA, [' '.join(x.strip().split()) for x in row[1:3]])
+        elif len(row) >= 1 and row[0]:
+            m = re.match(DISTRICT_PATTERN, unicode(row[0]), flags=re.I|re.UNICODE)
+            if m:
+                return (DISTRICT, m.group(1).strip().title())
+    except Exception, e:
+        print '>>>>', e
         # sys.exit(1)
     return (SKIP, None) 
 
-def parse(src, lines, region):
-    """
-    Assume the district name is fname (minus ext) until 
-    We meet a District pattern.
-    """
-    
-    district = fname(src)
+def parse(rows, region, district):
+    """Parse the given rows to extract centres data.
 
-    xs = []
+    :param: rows - Generator of (kind, value) tuples to parse
+    :param: region
+    :param: district
+    """
+
+    data = []
 
     while True:
         try:
-            kind, val = lines.next()
+            kind, val = rows.next()
 
             if kind is DATA:
-                xs.append([region, district] + val)
+                data.append([region, district] + val)
             elif kind is DISTRICT:
                 district = val
-
         except StopIteration:
             break
-    return xs
+    return data
 
 def open_wb(fname):
+    """Open workbook.
+    """
     with open(fname, 'rb') as fl:
         content = fl.read()
     return open_workbook(file_contents=content)
 
 def is_xls(src):
+    """Returns True if src is an Excel file (xls/xlsx).
+    """
     return re.match(r'^.+\.xlsx?$', src)
 
 def fname(src):
+    """Returns the filename or folder name of src.
+    """
     src = src.strip().replace('\\', '')
     while src.endswith('/'):
         src = src[:-1]
